2134b41caf647922e576aefa65b94e7295f13183
diehard-rohan <65865716+diehard-rohan@users.noreply.github.com>
2022-11-19 01:01:45 +0530

    Update setup.py

--- python-example-flask/setup.py
+++ python-example-flask/setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
 import os.path
 
-os.system('pip install keyrings.google-artifactregistry-auth==1.1.1')
+os.system('pip install keyrings.google-artifactregistry-auth')
 setup(name='acme.sql', version='0.1.1')
